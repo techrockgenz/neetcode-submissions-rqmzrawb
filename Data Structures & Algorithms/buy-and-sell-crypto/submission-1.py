3b19dc728fe84c4503984fade8695197e2a1fc1a
techrockgenz <techrock.genz@gmail.com>
2026-04-13 14:16:17 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        slow = 0
+        profit = 0
+        for fast in range(1, len(prices)):
+            profit = max (profit, prices[fast] - prices[slow])
+            if prices[fast] < prices[slow]:
+                slow = fast
+
+        return profit
