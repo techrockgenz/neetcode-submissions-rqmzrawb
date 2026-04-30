51dee5581671d0a7d59dbcee36bbf3e0dad9016e
techrockgenz <techrock.genz@gmail.com>
2026-04-30 07:44:55 +0530

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.py
@@ -0,0 +1,27 @@
+# n * 2 ^ n, n
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+
+            if total > target or i == len(candidates):
+                return
+
+            # include candidates[i]
+            cur.append(candidates[i])
+            dfs(i + 1, cur, total + candidates[i])
+            cur.pop()
+
+            # skip candidates[i]
+            while i + 1 < len(candidates) and candidates[i] == candidates[i+1]: # skip till we have same number
+                i += 1 
+            dfs(i + 1, cur, total) # different from earlier cobi sum, because we don't want
+                                   # to consider cur
+
+        dfs(0, [], 0)
+        return res
