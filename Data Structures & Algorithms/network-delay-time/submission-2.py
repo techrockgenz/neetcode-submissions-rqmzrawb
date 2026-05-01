4d2d1879883f00fc01b4e1eb81e2e50b8b63f05d
techrockgenz <techrock.genz@gmail.com>
2026-05-01 18:25:00 +0530

    Add: network-delay-time - submission-2

---
+++ Data Structures & Algorithms/network-delay-time/submission-2.py
@@ -0,0 +1,27 @@
+# E * log V , V^2 
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        edges = collections.defaultdict(list)
+        for src, dest, dis in times:
+            edges[src].append((dest, dis))
+        
+        minHeap = [(0, k)] # k is starting node
+        visit = set()
+        time = 0
+
+        while minHeap:
+            dis, src = heapq.heappop(minHeap)
+            if src in visit:
+                continue
+            visit.add(src)
+            time = dis
+
+            # Till here nothing, just pop from minHeap, visit and set distance
+            # After this if node not visited i.e. all BFS keep on pushing, with 
+            # distance from starting point i.e. dis + current distance nextDis
+            for nextSrc, nextDis in edges[src]:
+                if nextSrc not in visit:
+                    heapq.heappush(minHeap, (dis + nextDis, nextSrc))
+
+        return time if len(visit) == n else -1 # If connected
