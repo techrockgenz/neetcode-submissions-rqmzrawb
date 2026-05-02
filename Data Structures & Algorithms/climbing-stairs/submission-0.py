b9cefe8ca9b2e5572cd41fcd73ede354a105dd0c
techrockgenz <techrock.genz@gmail.com>
2026-05-02 11:49:24 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,12 @@
+# 2 ^ n top-down, brute force
+# n, top-down, memoization
+
+# n, n, bottom-up, Tabulation
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+
+        for i in range(n - 1):
+            one, two = one + two, one
+        return one
