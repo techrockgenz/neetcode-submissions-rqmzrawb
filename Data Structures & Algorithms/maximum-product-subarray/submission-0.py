8e84873204b4298df905cbd9f2d2872fd6dc4478
techrockgenz <techrock.genz@gmail.com>
2026-05-02 18:32:42 +0530

    Add: maximum-product-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-0.py
@@ -0,0 +1,17 @@
+# Brute force n^2
+# 
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        curMin = curMax = 1
+
+        for n in nums:
+            # if n == 0:
+            #     curMin = curMx = 1
+            #     continue
+            
+            temp = curMax * n
+            curMax = max(n * curMax, n * curMin, n)
+            curMin = min(temp, n * curMin, n)
+            res = max(res, curMax)
+        return res
