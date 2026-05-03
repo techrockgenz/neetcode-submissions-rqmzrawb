3757560311279463665b3ba8bcaa70ebf566d5d3
techrockgenz <techrock.genz@gmail.com>
2026-05-03 12:39:07 +0530

    Add: partition-equal-subset-sum - submission-10

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-10.py
@@ -0,0 +1,21 @@
+# Brute force 2 ^ n
+# n * sum(nums) / 2, n * sum(nums), since num is max 200, its good 
+# Space is also same, but using DP we can reduce it
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+
+        dp = set()
+        dp.add(0) # We take any number or don't take it, so this is for not taking
+        target = sum(nums) // 2
+        if target in nums: # Optimization
+            return True
+
+        # for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums)):
+            for t in dp.copy():
+                newTarget = t + nums[i]
+                if newTarget == target: return True
+                dp.add(newTarget)
+        return False    
