f5af4eacd981a586e8e320535d091a8b5c6c0a98
techrockgenz <techrock.genz@gmail.com>
2026-09-21 22:30:23 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.py
@@ -0,0 +1,15 @@
+# Don't add all elements first, same number reuse is not allowed
+# [2, 1, 4, 3] target 4.
+# 2 in array, then while checking it 2 + 2 = 4, wrong
+
+# n2, 1
+# n, n
+
+class Solution:
+    def twoSum(self, nums:List[int], target:int) -> List[int]:
+        prevMap = {} # val : index
+        for index, num in enumerate(nums):
+            diff = target - num
+            if diff in prevMap:
+                return [prevMap[diff], index]
+            prevMap[num] = index
