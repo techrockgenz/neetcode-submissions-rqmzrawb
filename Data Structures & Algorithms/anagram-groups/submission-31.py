7ad1f2183b94778fab88cd7a6448df3939faf77d
techrockgenz <techrock.genz@gmail.com>
2026-09-24 16:17:56 +0530

    Add: anagram-groups - submission-31

---
+++ Data Structures & Algorithms/anagram-groups/submission-31.py
@@ -0,0 +1,8 @@
+# m * n log n, m * n
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return [v for v in res.values()]
